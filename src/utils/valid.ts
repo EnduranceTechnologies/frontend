6e2da39ceeca013905b5583542171db8fee2433e
Clara Tavares <claratavaresilva@gmail.com>
2025-08-03 10:47:50 -0300

    feat: tela de login com carrosel

--- src/utils/valid.ts
+++ src/utils/valid.ts
@@ -86,3 +86,10 @@ export function isValidCNPJ(cnpj: string): boolean {
 
   return true;
 }
+
+export function emailValidator(email: string): boolean {
+  const reg =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+  return reg.test(email);
+}
